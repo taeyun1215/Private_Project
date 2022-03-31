7dd1b3ca85f944e78074978be563d1dc9ce03e6a
Lee tae yun <taeyun1215@naver.com>
2022-03-31 17:30:30 +0900

    22.03.31

---
+++ core/src/main/java/SpringBootCore/core/MemberApp.java
@@ -0,0 +1,18 @@
+package SpringBootCore.core;
+
+import SpringBootCore.core.member.Grade;
+import SpringBootCore.core.member.Member;
+import SpringBootCore.core.member.MemberService;
+import SpringBootCore.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
