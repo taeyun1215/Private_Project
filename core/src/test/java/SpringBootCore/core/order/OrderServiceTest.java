31ece4862e76ca8bdecb0bb484c9143a53ad270b
Lee tae yun <taeyun1215@naver.com>
2022-04-11 22:32:47 +0900

    22.04.11

--- core/src/test/java/SpringBootCore/core/order/OrderServiceTest.java
+++ core/src/test/java/SpringBootCore/core/order/OrderServiceTest.java
@@ -1,17 +1,19 @@
-package SpringBootCore.core.order;
+package SpringBootCore.core.member;
 
 import SpringBootCore.core.AppConfig;
 import SpringBootCore.core.member.Grade;
 import SpringBootCore.core.member.Member;
 import SpringBootCore.core.member.MemberService;
 import SpringBootCore.core.member.MemberServiceImpl;
+import SpringBootCore.core.order.Order;
+import SpringBootCore.core.order.OrderService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-//    MemberService memberService = new MemberServiceImpl();
+    //    MemberService memberService = new MemberServiceImpl();
 //    OrderService orderService = new OrderServiceImpl();
     MemberService memberService;
     OrderService orderService;
